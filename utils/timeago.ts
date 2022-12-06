cfc84d836e992b6da8c6cade1bbd16b5048e2797
JF Durand <jean-frederic.durand@qohash.com>
2022-12-06 17:07:11 -0500

    fix: allow isInTimeAgo to match minute format

--- utils/timeago.ts
+++ utils/timeago.ts
@@ -1,8 +1,12 @@
+const minFormat = /[0-9]{1,2}[ ]?min/
 const hFormat = /[0-9]h/
 const dFormat = /[0-9]d/
 
+// dateA can match min, hours and day format
+// timeAgo can match hours and day format (not min)
 export const isInTimeAgo = (dateA: string, timeago: string): boolean => {
   if (!timeago.length) return true
+  if (dateA.match(minFormat)) return true
   if (dateA.match(hFormat) && timeago.match(dFormat)) return true
   if (dateA.match(dFormat) && timeago.match(hFormat)) return false
   if (dateA.match(hFormat) && timeago.match(hFormat)) {
